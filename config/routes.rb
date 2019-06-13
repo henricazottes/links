1b26175ae18145a6e6ae14cfb530770be3e40faf
Henri Cazottes <henri.cazottes@gmail.com>
2019-06-13 11:14:12 +0200

    Adding Fomantic salt to views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :links
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'links#index'
 end
