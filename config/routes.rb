c85c5348dcc2ef5f2bfe9fcc9ddef761fc6a79a3
Henri Cazottes <henri.cazottes@gmail.com>
2019-07-22 13:36:30 +0200

    Adding username to registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
   resources :links
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'links#index'
 end
